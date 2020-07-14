e0afa416de2fd75d19fa6a94dc85793894adc28f
Malek <maleka@alumni.cmu.edu>
2020-07-14 03:59:47 +0300

    Setup basic nav mesh.

---
+++ Assets/MonsterNav.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterNav : MonoBehaviour
+{
+
+    public Camera myCam;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
