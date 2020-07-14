4050d455134830034b05d0777fbef378ab2d2d3f
Malek <maleka@alumni.cmu.edu>
2020-07-15 01:29:12 +0300

    FLoating animations

--- Assets/MonsterNav.cs
+++ Assets/MonsterNav.cs
@@ -1,11 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 public class MonsterNav : MonoBehaviour
 {
 
     public Camera myCam;
     public NavMeshAgent agent;
+
+    [Header("Float Settings")]
+    public float maxHeight = 0.5f;
+    public float minHeight = 0.0f;
+    public float duration = 4.0f;
+
+    private void Start()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, minHeight, this.transform.position.z);
+        Sequence s = DOTween.Sequence();
+        s.Append(this.transform.DOMoveY(maxHeight, duration));
+        s.Append(this.transform.DOMoveY(minHeight, duration));
+        s.SetLoops(-1, LoopType.Restart);
+    }
 
     // Update is called once per frame
     void Update()
