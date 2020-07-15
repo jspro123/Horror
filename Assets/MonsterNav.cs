625766b87d36439965f8452eb74531541b7668f5
Malek <maleka@alumni.cmu.edu>
2020-07-15 22:47:09 +0300

    Small pot

--- Assets/MonsterNav.cs
+++ Assets/MonsterNav.cs
@@ -6,15 +6,24 @@ public class MonsterNav : MonoBehaviour
 {
 
     public Camera myCam;
-    public NavMeshAgent agent;
+
+    public enum MonsterState { Random, Chase, Stop};
+
+    [Header("Monster Settings")]
+    public float monsterWalkSpeed = 1.0f;
+    public float monsterChaseSpeed = 4.0f;
 
     [Header("Float Settings")]
     public float maxHeight = 0.5f;
     public float minHeight = 0.0f;
     public float duration = 4.0f;
 
+    private NavMeshAgent agentProperties;
+    private MonsterState currentState = MonsterState.Random;
+
     private void Start()
     {
+        agentProperties = GetComponent<NavMeshAgent>();
         this.transform.position = new Vector3(this.transform.position.x, minHeight, this.transform.position.z);
         Sequence s = DOTween.Sequence();
         s.Append(this.transform.DOMoveY(maxHeight, duration));
@@ -22,9 +31,15 @@ public class MonsterNav : MonoBehaviour
         s.SetLoops(-1, LoopType.Restart);
     }
 
+    public void SwitchState(MonsterState newState)
+    {
+        currentState = newState;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
@@ -32,8 +47,9 @@ public class MonsterNav : MonoBehaviour
 
             if(Physics.Raycast(ray, out hit))
             {
-                agent.SetDestination(hit.point);
+                agentProperties.SetDestination(hit.point);
             }
         }
+        */
     }
 }
