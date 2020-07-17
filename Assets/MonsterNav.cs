03374bc7815a505ec68405e2c97cde6730f11869
Malek <maleka@alumni.cmu.edu>
2020-07-17 21:38:44 +0300

    Forgot to push last time.

--- Assets/MonsterNav.cs
+++ Assets/MonsterNav.cs
@@ -4,15 +4,14 @@ using DG.Tweening;
 
 public class MonsterNav : MonoBehaviour
 {
-    // public Camera myCam;
-    public GameObject myCam;
 
-    public enum MonsterState { Random, Chase, Stop};
+    public Camera myCam;
+
+    public enum MonsterState { Random, Chase, Stop };
 
     [Header("Monster Settings")]
     public float monsterWalkSpeed = 1.0f;
     public float monsterChaseSpeed = 4.0f;
-    public float monsterCaptureThreshold = 1.0f;
 
     [Header("Float Settings")]
     public float maxHeight = 0.5f;
@@ -22,48 +21,39 @@ public class MonsterNav : MonoBehaviour
     private NavMeshAgent agentProperties;
     private MonsterState currentState = MonsterState.Random;
 
-    public void FollowMove(float speed) {
-        var monsterTransform = GetComponent<Transform>();
-        var monsterPosition = monsterTransform.position;
-        var targetPosition = myCam.transform.position;
-        int dx = 0;
-        int dz = 0; 
-        if (targetPosition.x > monsterPosition.x) {
-            dx = 1;
-        } else if (targetPosition.x < monsterPosition.x) {
-            dx = -1;
-        }
-        if (targetPosition.z > monsterPosition.z) {
-            dz = 1;
-        } else if (targetPosition.z < monsterPosition.z) {
-            dz = -1;
-        }
-        var newPosition = new Vector3(monsterPosition.x + dx*speed, monsterPosition.y, monsterPosition.z + dz*speed);
-        monsterTransform.DOMove(newPosition, duration);
-    }
-
     private void Start()
     {
-        // agentProperties = GetComponent<NavMeshAgent>();
-        // var trans = GetComponent<Transform>();
-        // this.transform.position = new Vector3(this.transform.position.x, minHeight, this.transform.position.z);
-        // monster.transform.DOMoveX(trans.transform.position.x +5, 5);
-        // Sequence s = DOTween.Sequence();
-        // s.Append(this.transform.DOMoveY(340.06, 2));
-        // s.Append(this.transform.DOMoveY(minHeight, duration));
-        // s.SetLoops(-1, LoopType.Restart);
-        FollowMove(monsterWalkSpeed);
+        agentProperties = GetComponent<NavMeshAgent>();
+        this.transform.position = new Vector3(this.transform.position.x, minHeight, this.transform.position.z);
+        Sequence s = DOTween.Sequence();
+        s.Append(this.transform.DOMoveY(maxHeight, duration));
+        s.Append(this.transform.DOMoveY(minHeight, duration));
+        s.SetLoops(-1, LoopType.Restart);
     }
 
     public void SwitchState(MonsterState newState)
     {
         currentState = newState;
+
+        switch (currentState)
+        {
+            case MonsterState.Random:
+                agentProperties.speed = monsterWalkSpeed;
+                break;
+
+            case MonsterState.Chase:
+                agentProperties.speed = monsterChaseSpeed;
+                break;
+
+            case MonsterState.Stop:
+                agentProperties.speed = 0;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowMove(monsterWalkSpeed);
         /*
         if (Input.GetMouseButtonDown(0))
         {
@@ -76,5 +66,7 @@ public class MonsterNav : MonoBehaviour
             }
         }
         */
+
+        agentProperties.SetDestination(myCam.transform.position);
     }
 }
