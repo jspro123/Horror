bcb202a715725ad87fbe0a360a5ab2cc01c27537
Zan Naeem <znaeem@qatar.cmu.edu>
2020-07-19 23:37:11 +0300

    added interaction flag for mirrors

---
+++ Assets/MagicMirror/Prefab/Interact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject grabber;
+    public float minDistance = 5f;
+    public bool triggered = false;
+    private float distance;
+
+    void OnMouseDown() {
+        distance = Vector3.Distance(this.transform.position, grabber.transform.position);
+        if (distance <= minDistance) {
+            triggered = !triggered;
+            Debug.Log(triggered);
+        }
+    }
+}
