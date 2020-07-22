3ffff7d2d869058ec104b72a746f61f4179bd49b
Malek <maleka@alumni.cmu.edu>
2020-07-22 10:10:12 +0300

    Better mirrors

--- Assets/ImportedModels/MagicMirror/Prefab/MirrorReflectionScript.cs
+++ Assets/ImportedModels/MagicMirror/Prefab/MirrorReflectionScript.cs
@@ -5,11 +5,21 @@ public class MirrorReflectionScript : MonoBehaviour
 {
     private MirrorCameraScript childScript;
     private Camera mainCamera;
+    private BoxCollider collider;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    private const int NUMBER_CHECKS = 15;
+    private bool visible = false;
+    private float timeElapsed = 0;
+    private const float intervalLength = 1.0f;
 
     private void Start()
     {
         childScript = gameObject.transform.parent.gameObject.GetComponentInChildren<MirrorCameraScript>();
         mainCamera = Camera.main;
+        collider = mainCamera.GetComponent<BoxCollider>();
 
         if (childScript == null)
         {
@@ -17,16 +27,74 @@ public class MirrorReflectionScript : MonoBehaviour
         }
     }
 
+    private void RandomCheckIfVisible()
+    {
+        for (int i = 0; i < NUMBER_CHECKS; i++)
+        {
+            Vector3 randomPoint = new Vector3(
+                Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+                Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                Random.Range(collider.bounds.min.z, collider.bounds.max.z));
+
+            Vector3 rayDirection = this.transform.position - randomPoint;
+            RaycastHit hit;
+            Ray ray = new Ray(mainCamera.transform.position, rayDirection);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                if (hit.transform.gameObject.tag == this.tag)
+                {
+                    visible = true;
+                    return;
+                }
+            }
+        }
+
+        visible = false;
+    }
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > intervalLength)
+        {
+            timeElapsed = 0;
+            RandomCheckIfVisible();
+        }
+    }
+
     private void OnWillRenderObject()
     {
-        Vector3 vec = mainCamera.WorldToViewportPoint(this.transform.position);
-        RaycastHit hit;
+        /*
+        if (visible) { childScript.RenderMirror(); }
+        else
+        {
+            Vector3 rayDirection = this.transform.position - mainCamera.transform.position;
+            RaycastHit hit;
+
+            if (Physics.Raycast(mainCamera.transform.position, rayDirection, out hit, layerMask))
+            {
+                if (hit.transform.gameObject.tag == this.tag)
+                {
+                    childScript.RenderMirror();
+                }
+            }
+        }
+        */
+
         Vector3 rayDirection = this.transform.position - mainCamera.transform.position;
-        if (Physics.Raycast(mainCamera.transform.position, rayDirection, out hit))
+        RaycastHit hit;
+        Ray ray = new Ray(mainCamera.transform.position, rayDirection);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
             if (hit.transform.gameObject.tag == this.tag)
             {
                 childScript.RenderMirror();
+            } else
+            {
+                RandomCheckIfVisible();
+                if(visible) { childScript.RenderMirror(); }
             }
         }
     }
