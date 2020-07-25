9f493f9f31a2c3e0cf1a6a66e82a79590b083ecc
Malek <maleka@alumni.cmu.edu>
2020-07-25 23:11:56 +0300

    Lose screens.

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    public Camera newCam;
+    private float elapsed = 0;
+    
+    void Update()
+    {
+        if (newCam)
+        {
+            elapsed += Time.deltaTime;
+            if(elapsed > 10)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                newCam.gameObject.SetActive(true);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
